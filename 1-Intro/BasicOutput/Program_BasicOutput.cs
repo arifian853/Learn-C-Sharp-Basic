d72e902f7d9df750b015988740602856b62699ff
arifian853 <arifiansaputra43@gmail.com>
2022-05-13 13:24:24 +0700

    Initial commit

---
+++ 1-Intro/BasicOutput/Program_BasicOutput.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace BasicOutput
+{
+  class Program_BasicOutput
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      Console.WriteLine("Hello Indonesia !");    
+      Console.WriteLine("Hello My Friend");
+      string a = "Arifian";    
+      string b = " Saputra";
+      Console.WriteLine("Hello " + a);
+      Console.WriteLine("Good Morning, Mr. " + a + b);
+      // Below is simple arithmetic equation
+      Console.WriteLine(1 + 1);  
+      Console.WriteLine(2 - 1);  
+      Console.WriteLine(2 * 3);
+      Console.WriteLine(50 / 10);
+      Console.WriteLine(20 % 3);  
+      int x = 5, y = 4, z = 10;
+      Console.Write(x + y * z);
+    }
+  }
+}
